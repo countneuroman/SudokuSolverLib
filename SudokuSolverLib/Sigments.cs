128996f6e0065bcbf15795be203a707d05b8fcc1
Kartoshkin-btw <utkinana2011@mail.ru>
2022-04-13 10:49:01 +0300

    Добавил файлы библиотеки

---
+++ SudokuSolverLib/Sigments.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SudokuSolverLib
+{
+    static class Sigments
+    {
+        static int[,] first = new int[2, 3] { { 0, 1, 2 }, { 0, 1, 2 } };
+        static int[,] second = new int[2, 3] { { 0, 1, 2 }, { 3, 4, 5 } };
+        static int[,] third = new int[2, 3] { { 0, 1, 2 }, { 6, 7, 8 } };
+        static int[,] fourth = new int[2, 3] { { 3, 4, 5 }, { 0, 1, 2 } };
+        static int[,] fifth = new int[2, 3] { { 3, 4, 5 }, { 3, 4, 5 } };
+        static int[,] sixth = new int[2, 3] { { 3, 4, 5 }, { 6, 7, 8 } };
+        static int[,] seventh = new int[2, 3] { { 6, 7, 8 }, { 0, 1, 2 } };
+        static int[,] eighth = new int[2, 3] { { 6, 7, 8 }, { 3, 4, 5 } };
+        static int[,] ninth = new int[2, 3] { { 6, 7, 8 }, { 6, 7, 8 } };
+        static List<int[,]> sigments = new List<int[,]> { first, second, third, fourth, fifth, sixth, seventh, eighth, ninth };
+        public static int[,] GetSigment(int row, int column)
+        {
+            if (row >= 0 && row < 9 && column >= 0 && column < 9)
+            {
+                foreach (int[,] positions in sigments)
+                {
+                    for (int i = 0; i < 3; i++)
+                    {
+                        if (positions[0, i] == row)
+                        {
+                            for (int j = 0; j < 3; j++)
+                            {
+                                if (positions[1, j] == column)
+                                {
+                                    return positions;
+                                }
+                            }
+                        }
+                    }
+                }
+                throw new Exception("Error");
+            }
+            else
+            {
+                throw new Exception("Error");
+            }
+        }
+    }
+}
---
+++ SudokuSolverLib/Solver.cs
@@ -0,0 +1,107 @@
+﻿/// <summary>
+/// Sudoku solver
+/// </summary>
+namespace SudokuSolverLib
+{
+    /// <summary>
+    /// This class solves as array of sudoku.
+    /// </summary>
+    public class Solver
+    {
+        /// <summary>
+        /// Solves sudoku.
+        /// </summary>
+        /// <param name="sudoku">Array. Null values = 0.</param>
+        /// <returns>Returns the solved sudoku. If an exception or cannot be solved sudoku, returns int[0, 0] = -1.</returns>
+        public int[,] Solve(int[,] sudoku)
+        {
+            try
+            {
+                int[,] fakeSudoku = (int[,])sudoku.Clone();
+                for (int row = 0; row < 9; row++)
+                {
+                    for (int column = 0; column < 9; column++)
+                    {
+                        if (sudoku[row, column] == 0)
+                        {
+                            for (int value = 1; value < 10; value++)
+                            {
+                                if (Possibility(sudoku, row, column, value))
+                                {
+                                    fakeSudoku[row, column] = value;
+                                    int[,] result = Solve(fakeSudoku);
+                                    if (result[0, 0] != -1)
+                                    {
+                                        return result;
+                                    }
+                                }
+                                if (sudoku[row, column] == 0 && value == 9)
+                                {
+                                    return new int[,] { { -1 } };
+                                }
+                            }
+                        }
+                    }
+                }
+                return sudoku;
+            }
+            catch
+            {
+                return new int[,] { { -1 } };
+            }
+        }
+        bool Possibility(int[,] sudoku, int row, int column, int value)
+        {
+            if (HorizontalPossibility(sudoku, row, value) && VerticalPossibility(sudoku, column, value) && SigmentPossibility(sudoku, row, column, value))
+                return true;
+            return false;
+        }
+        bool HorizontalPossibility(int[,] sudoku, int row, int value)
+        {
+            bool result = true;
+            for (int i = 0; i < 9; i++)
+            {
+                if (sudoku[row, i] == value)
+                {
+                    result = false;
+                    break;
+                }
+
+            }
+            return result;
+        }
+        bool VerticalPossibility(int[,] sudoku, int column, int value)
+        {
+            bool result = true;
+            for (int i = 0; i < 9; i++)
+            {
+                if (sudoku[i, column] == value)
+                {
+                    result = false;
+                    break;
+                }
+
+            }
+            return result;
+        }
+        bool SigmentPossibility(int[,] sudoku, int row, int column, int value)
+        {
+            int[,] sigment = Sigments.GetSigment(row, column);
+            bool result = true;
+            for (int i = 0; i < 3; i++)
+            {
+                if (!result)
+                    break;
+                for (int j = 0; j < 3; j++)
+                {
+                    if (sudoku[sigment[0, i], sigment[1, j]] == value)
+                    {
+                        result = false;
+                        break;
+                    }
+                }
+            }
+            return result;
+        }
+    }
+}
